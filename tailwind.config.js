2dc356237b49669321ae54e1b54c86d120cb630b
sayidina ahmadal qososyi <58449456+oendnsk675@users.noreply.github.com>
2022-08-21 07:42:10 +0800

    add responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {},
+    screens: {
+        'xsm': {'max': '383px'},
+        'smx': {'max': '530px'}
+    }
   },
   plugins: [],
 }
